7f4823e38826f0d8d14af37787e2c112f9ba683a
TymRail <tymrail@163.com>
2016-10-16 22:39:10 +0800

    Test Git In Intellij IDEA

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,6 +21,8 @@ public class Main {
         scan.close();
     }
 
+    //just test
+
     public static String dealInput(String input,String exp) {
         plusMinusExpressions<String,Integer,String,Integer> tempPme = new plusMinusExpressions<>();
         if (input.substring(0,1).equals("!")) {
